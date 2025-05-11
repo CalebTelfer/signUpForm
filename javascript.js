847a742d57c41d65ed6e7206f66059ca1a8f9f20
caleb <calebtelfe@gmail.com>
2025-05-10 20:23:34 -0400

    error check input. passwords must match too.

--- javascript.js
+++ javascript.js
@@ -1,9 +1,15 @@
 const form = document.getElementById("registerForm");
 const submitButton = document.getElementById("submitButton");
+const passwordBox = document.getElementById("password");
+const confirmPasswordBox = document.getElementById("confirmPassword");
 
 submitButton.addEventListener("click", function() {
-    if (form.checkValidity()) {
+    if (form.reportValidity()) {
+        if(passwordBox.textContent == confirmPasswordBox.textContent)
         form.submit();
+    } else {
+        passwordBox.style.borderColor = "red";
+        confirmPasswordBox.style.borderColor = "red";
     }
 });
 
